96a904642518963d3edfb5d52c580c93c3fa2954
RobajziCloud <78492876+RobajziCloud@users.noreply.github.com>
2025-08-08 10:16:39 +0200

    Delete src directory

--- src/components/Logo.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Logo() {
-  return (
-    <h1 className="text-4xl font-black tracking-tight">
-      LANG<span className="text-green-400">.</span>TRAINER
-    </h1>
-  );
-}
