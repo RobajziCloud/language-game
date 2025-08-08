91b9480830b1d388049732cfcc19c0651f8b8e5f
RobajziCloud <78492876+RobajziCloud@users.noreply.github.com>
2025-08-08 10:33:47 +0200

    Delete src directory

--- src/components/LoadingScreen.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function LoadingScreen() {
-  return (
-    <div className="bg-black text-white h-screen flex items-center justify-center text-xl animate-pulse">
-      Načítání...
-    </div>
-  );
-}
--- src/components/Logo.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Logo() {
-  return <div className="text-4xl font-bold">Lang Trainer</div>;
-}
