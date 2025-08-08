16732e54b4f57ab6d38c153aa5c9cfcc1caff0db
RobajziCloud <78492876+RobajziCloud@users.noreply.github.com>
2025-08-08 16:10:38 +0200

    Delete app directory

--- app/hooks/useSentenceSource.ts
+++
@@ -1,4 +0,0 @@
-// Hook to load sentences from JSON shards
-export function useSentenceSource(level) {
-  return { buffer: [], prefetching: false, setLevel: () => {}, getNextSentence: () => null };
-}
--- app/page.tsx
+++
@@ -1,5 +0,0 @@
-// Lang Trainer main file with integrated useSentenceSource
-import React from 'react';
-export default function Page() {
-  return <div>Lang Trainer App Placeholder</div>;
-}
